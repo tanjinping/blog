3e7be7ab7137ea19acbe4287999ebca94e268ede
tanjinping <tanjinping@tanjinpingdeiMac.local>
2020-04-11 15:38:09 +0800

    master

---
+++ app.js
@@ -0,0 +1,15 @@
+const serverHandle = (req,res) => {
+    //设置返回格式
+    res.setHeader('Content-type','application/json')
+
+    const resData = {
+        name:"shuangyue",
+        site:"imooc"
+    }
+
+    res.end(
+        JSON.stringify(resData)
+    )
+}
+
+module.exports = serverHandle
