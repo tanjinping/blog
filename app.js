5f960a96e06499cfb8601eb8825caa35f37f76e8
tanjinping <tjp@prismslight.com>
2020-04-11 18:13:48 +0800

    添加数据模型

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const handleBlogRouter = require('./src/router/blog');
 const handleUserRouter = require('./src/router/user');
+const querystring = require('querystring');
 
 const serverHandle = (req, res) => {
     //设置返回格式
     res.setHeader('Content-type', 'application/json');
+
     //获取path
     const url = req.url;
     req.path = url.split('?')[0];
+
+    //解析query
+    req.query = querystring.parse(url.split('?')[1]);
 
     //处理blog路由
     const blogData = handleBlogRouter(req, res);
---
+++ src/controller/blog.js
@@ -0,0 +1,37 @@
+const getList = (author, keyword) => {
+    //假数据
+    return [
+        {
+            id: 1,
+            title: '标题1',
+            createTime: 1586599211553,
+            content: "内容1",
+            author: "张三"
+        },
+        {
+            id: 2,
+            title: '标题2',
+            createTime: 1586599275160,
+            content: "内容2",
+            author: "李四"
+        },
+        {
+            id: 3,
+            title: '标题3',
+            createTime: 1586599281260,
+            content: "内容3",
+            author: "王武"
+        },
+        {
+            id: 4,
+            title: '标题4',
+            createTime: 1586599287160,
+            content: "内容4",
+            author: "黎明"
+        }
+    ]
+};
+
+module.exports = {
+    getList
+};
