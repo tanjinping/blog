76afa510ed3fd287331123f5783bbb5b0f5fc4a0
tanjinping <tjp@prismslight.com>
2020-04-11 17:42:06 +0800

    添加路由

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-const serverHandle = (req,res) => {
+const handleBlogRouter = require('./src/router/blog');
+const handleUserRouter = require('./src/router/user');
+
+const serverHandle = (req, res) => {
     //设置返回格式
-    res.setHeader('Content-type','application/json');
+    res.setHeader('Content-type', 'application/json');
+    //获取path
+    const url = req.url;
+    req.path = url.split('?')[0];
 
-    const resData = {
-        name:"shuangyue",
-        site:"imooc"
-    };
+    //处理blog路由
+    const blogData = handleBlogRouter(req, res);
+    if (blogData) {
+        res.end(JSON.stringify(blogData));
+        return;
+    }
+    const userData = handleUserRouter(req, res);
+    if (userData) {
+        res.end(
+            JSON.stringify(userData)
+        );
+        return;
+    }
 
-    res.end(
-        JSON.stringify(resData)
-    )
+    //未命中路由
+    res.writeHead(404, {"Content-type": "text/plain"});
+    res.write("404 NOT Found\n");
+    res.end();
 };
 
 module.exports = serverHandle;
+
+//process.env.NODE_ENV
