e71343b0576d64164f9c28c9f5e0f44dcec36556
tanjinping <tjp@prismslight.com>
2020-04-23 23:55:35 +0800

    API对接mysql

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -1,35 +1,17 @@
+const {exec} = require('../db/mysql');
+
 const getList = (author, keyword) => {
-    //假数据
-    return [
-        {
-            id: 1,
-            title: '标题1',
-            createTime: 1586599211553,
-            content: "内容1",
-            author: "张三"
-        },
-        {
-            id: 2,
-            title: '标题2',
-            createTime: 1586599275160,
-            content: "内容2",
-            author: "李四"
-        },
-        {
-            id: 3,
-            title: '标题3',
-            createTime: 1586599281260,
-            content: "内容3",
-            author: "王武"
-        },
-        {
-            id: 4,
-            title: '标题4',
-            createTime: 1586599287160,
-            content: "内容4",
-            author: "黎明"
-        }
-    ]
+    let sql = `select * from blogs where 1=1 `;
+    if (author) {
+        sql += `and author='${author}' `;
+    }
+    if (keyword) {
+        sql += `and title like '%${keyword}%' `;
+    }
+    sql += `order by createtime desc;`;
+
+    //返回 promise
+    return exec(sql);
 };
 
 const getDetail = (id) => {
@@ -60,5 +42,5 @@ const deleteBlog = (id) => {
 };
 
 module.exports = {
-    getList, getDetail, newBlog, updateBlog,deleteBlog
+    getList, getDetail, newBlog, updateBlog, deleteBlog
 };
